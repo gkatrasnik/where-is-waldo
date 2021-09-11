5dab8705d36ff3a24fcd5314b6cac941657f745a
Gasper Katrasnik <gkatrasnik@gmail.com>
2021-09-11 17:36:41 +0200

    images added to characters

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,11 +1,51 @@
 // coordinates are relative to window size, calculated to 3 decimal places
 export const characters = [
-  //{ id: 0, name: "Mr.Peanutbuter", position: [0.911, 0.224] },
-  { id: 1, name: "Ned Flanders", position: [0.176, 0.366] },
-  //{ id: 2, name: "Bugs Bunny", position: [0.43, 0.462] },
-  //{ id: 3, name: "Sonic the Hedgehog", position: [0.757, 0.498] },
-  { id: 4, name: "Jake the dog", position: [0.781, 0.536] },
-  { id: 5, name: "Waldo", position: [0.283, 0.716] },
-  //{ id: 6, name: "Fred Flintstone", position: [0.09, 0.779] },
-  //{ id: 7, name: "Rick Sanchez", position: [0.684, 0.901] },
+  {
+    id: 0,
+    name: "Mr.Peanutbuter",
+    position: [0.911, 0.224],
+    url: "/images/mrpeanutbutter.png",
+  },
+  {
+    id: 1,
+    name: "Ned Flanders",
+    position: [0.176, 0.366],
+    url: "/images/ned_flanders.png",
+  },
+  {
+    id: 2,
+    name: "Bugs Bunny",
+    position: [0.43, 0.462],
+    url: "/images/bugs_bunny.png",
+  },
+  {
+    id: 3,
+    name: "Sonic the Hedgehog",
+    position: [0.757, 0.498],
+    url: "/images/sonic_the_hedgehog.png",
+  },
+  {
+    id: 4,
+    name: "Jake the dog",
+    position: [0.781, 0.536],
+    url: "/images/jake_the_dog.png",
+  },
+  {
+    id: 5,
+    name: "Waldo",
+    position: [0.283, 0.716],
+    url: "/images/waldo.png",
+  },
+  {
+    id: 6,
+    name: "Fred Flintstone",
+    position: [0.09, 0.779],
+    url: "/images/fred_flintstone.png",
+  },
+  {
+    id: 7,
+    name: "Rick Sanchez",
+    position: [0.684, 0.901],
+    url: "/images/rick_sanchez.png",
+  },
 ];
